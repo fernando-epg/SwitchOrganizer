cca9841a37d6c4bddec9bbff972260b5e558b8f6
Fernando <jumbonionga@gmail.com>
2019-02-14 17:48:41 -0600

    Implementation of the File lister as class

--- src/com/fernandoperez/Main.java
+++ src/com/fernandoperez/Main.java
@@ -1,10 +1,9 @@
 package com.fernandoperez;
 
-import java.io.File;
-
 public class Main {
 
     public static void main(String[] args) {
+
         // Test directory instead of Drive letter
         String originalLocation = "C:\\users\\ferna\\Desktop\\Switch MicroSD";
         // Edit to add the Nintendo folder
@@ -12,24 +11,8 @@ public class Main {
 
         System.out.println(originalLocation); // Testing purpose display
 
-        fileLister(nintendoLocation);
+        FileLister fileLister = new FileLister(nintendoLocation);
+        fileLister.lister();
     }
 
-    private static void fileLister(String path) {
-        File directory = new File(path);
-        File[] fList = directory.listFiles();
-        if (fList != null) {
-
-            for (File file : fList) {
-                if (file.isDirectory()) {
-                    fileLister(file.getAbsolutePath());
-                } else {
-                    System.out.println(file.getAbsolutePath());
-                    //System.out.println(file.getName());
-                }
-            }
-        } else {
-            System.out.println("No directory found");
-        }
-    }
 }
